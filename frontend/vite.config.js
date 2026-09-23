87b32eb040d90d26a0d2d6c68abfc9328a46cfd4
HARSH SONPURIA <sonpuriaharsh@gmail.com>
2026-09-23 19:58:24 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['threatbye.onrender.com'],
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['threatbye-abc123.onrender.com'],
   },
 })
